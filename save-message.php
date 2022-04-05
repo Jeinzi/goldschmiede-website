51f74500c1fd8292dd171c3a8954232cd29d2b38
Jeinzi <jeinzi@gmx.de>
2022-04-05 16:07:35 +0200

    Add basic spam filter

--- save-message.php
+++ save-message.php
@@ -6,11 +6,33 @@ $text = $_POST['text'];
 
 include("include/utility.php");
 
+// Must be lowercase!
+$spamKeywords = array(
+    "crypto",
+    "bitcoin",
+    " ceo ",
+    " seo ",
+);
 
-// Generate message directory.
+
+// Generate message directories.
 $messagePath = "messages/";
 if (!file_exists($messagePath)) {
     mkdir($messagePath);
+}
+$spamPath = $messagePath . "spam/";
+if (!file_exists($spamPath)) {
+    mkdir($spamPath);
+}
+
+// Sort out spam.
+$isSpam = false;
+$textLowercase = strtolower($text);
+foreach ($spamKeywords as $keyword) {
+    if (str_contains($textLowercase, $keyword)) {
+        $isSpam = true;
+        break;
+    }
 }
 
 // Compile notification text.
@@ -18,51 +40,16 @@ $notificationText = "Name: " . $name . "\n" .
                     "Kontakt: " . $contact . "\n" .
                     "Text:\n" . $text;
 
-
-// Send email.
-$connection = connectdB();
-$query = $connection->prepare("select * from freya.settings;");
-$result = $query->execute();
-if ($result === false) {
-	$mailSuccess = false;
-}
-$settingsRow = $query->fetch(PDO::FETCH_ASSOC);
-
-
-$query = $connection->prepare("select * from freya.contactEmails;");
-$result = $query->execute();
-if ($result === false) {
-	$mailSuccess = false;
-}
-else {
-    $firstRow = true;
-    $recipient = '';
-    $rows = $query->fetchAll(PDO::FETCH_ASSOC);
-    foreach ($rows as $row) {
-        if ($firstRow) {
-            $firstRow = false;
-        }
-        else {
-            $recipient .= ",";
-        }
-        $recipient .= "";
-    }
-
-    $subject = $settingsRow["contactSubject"];
-    $message = $notificationText;
-    $header = 'From: ' . $settingsRow["contactFrom"] . "\r\n" .
-    'Reply-To: ' . $settingsRow["contactReplyTo"] . "\r\n" . // TODO: Needed?
-    'Date: ' . date('r') . "\r\n" .
-    'Sender: ' . $settingsRow["contactSender"] . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-   
-    $mailSuccess = mail($recipient, $subject, $message, $header);
-}
-
 // Write to file.
 $fileSuccess = true;
 $fileName = date("c") . ".txt";
-$path = $messagePath . $fileName;
+if ($isSpam) {
+    $path = $spamPath . $fileName;
+}
+else {
+    $path = $messagePath . $fileName;
+}
+
 try {
     $file = fopen($path, "a");
     fwrite($file, $notificationText);
@@ -73,8 +60,50 @@ catch (Exception $e) {
 }
 
 
+if (!$isSpam) {
+    // Send email.
+    $connection = connectdB();
+    $query = $connection->prepare("select * from freya.settings;");
+    $result = $query->execute();
+    if ($result === false) {
+        $mailSuccess = false;
+    }
+    $settingsRow = $query->fetch(PDO::FETCH_ASSOC);
 
-if ($fileSuccess && $mailSuccess) {
+
+    $query = $connection->prepare("select * from freya.contactEmails;");
+    $result = $query->execute();
+    if ($result === false) {
+        $mailSuccess = false;
+    }
+    else {
+        $firstRow = true;
+        $recipient = '';
+        $rows = $query->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($rows as $row) {
+            if ($firstRow) {
+                $firstRow = false;
+            }
+            else {
+                $recipient .= ",";
+            }
+            $recipient .= "";
+        }
+
+        $subject = $settingsRow["contactSubject"];
+        $message = $notificationText;
+        $header = 'From: ' . $settingsRow["contactFrom"] . "\r\n" .
+        'Reply-To: ' . $settingsRow["contactReplyTo"] . "\r\n" . // TODO: Needed?
+        'Date: ' . date('r') . "\r\n" .
+        'Sender: ' . $settingsRow["contactSender"] . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+        $mailSuccess = mail($recipient, $subject, $message, $header);
+    }
+}
+
+
+if ($fileSuccess && ($isSpam || $mailSuccess)) {
     header('Location: thanks?success=1');
 }
 else {
