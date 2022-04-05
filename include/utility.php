421632d8d010da243734e70f8f9f908c0d8a5094
Jeinzi <jeinzi@gmx.de>
2022-04-05 02:55:22 +0200

    Improve alert box

--- include/utility.php
+++ include/utility.php
@@ -1,7 +1,7 @@
 <?php
 // Adds a fixed alert to the bottom of the page.
 function alert($text) {
-	echo '<div class="alert alert-danger alert-dismissible mx-auto" role="alert">';
+	echo '<div class="alert alert-danger alert-dismissible alert-static mx-auto" role="alert">';
 	echo $text;
 	echo '<button type="button" class="close" data-dismiss="alert" aria-label="Close">
 	<span aria-hidden="true">&times;</span>
