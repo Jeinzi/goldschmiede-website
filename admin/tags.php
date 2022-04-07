01549146fbf4da8e6071e6f46dba44588528d92c
Jeinzi <jeinzi@gmx.de>
2022-04-08 01:05:12 +0200

    Improve mobile version

--- admin/tags.php
+++ admin/tags.php
@@ -13,9 +13,7 @@ if (!isset($_SESSION['goldsmithLoggedIn'])) {
     <?php include("../include/head.php"); ?>
     <style>
         #button-delete {
-            background-color: #dc3545;
-            border-color: #dc3545;
-            width: 10%;
+            width: 7rem;
         }
 
         .activeBadge {
@@ -50,14 +48,14 @@ if (!isset($_SESSION['goldsmithLoggedIn'])) {
     <div class="row">
         <div class="col-12">
             <h1 class="main-heading border-bottom">Tags
-                <button id="button-add-tag" title="Tags hinzufügen" class="btn btn-outline-input btn-outline-secondary" style="float: right;">
+                <button id="button-add-tag" title="Tags hinzufügen" class="btn btn-outline-input btn-outline-secondary" style="float: right;margin-top: 0.35rem;">
                     <img src="/svg/plus.svg" alt="Plus-Zeichen">
                 </button>
             </h1>
         </div>
     </div>
     <div class="row">
-        <div id="tag-container" class="col-md-4">
+        <div id="tag-container" class="col-md-4 mb-3">
             <?php
                 try {
                     $connection = connectdB();
@@ -85,28 +83,36 @@ if (!isset($_SESSION['goldsmithLoggedIn'])) {
                 }
       ?></div>
         <div class="col-md-8">
-            <div class="input-group mb-2">
-                <div class="input-group-prepend">
-                    <div id="tag-name-prepend" class="input-group-text">
-                        Name
+            <div class="row">
+                <div class="col">
+                    <div class="input-group mb-2">
+                        <div class="input-group-prepend">
+                            <div id="tag-name-prepend" class="input-group-text">
+                                Name
+                            </div>
+                        </div>
+                        <input type="text" id="input-name" class="form-control" aria-labelledby="tag-name-prepend">
+                        <div class="input-group-append">
+                            <input id="input-color" type="color" style="height: 100%;">
+                        </div>
+                        <div class="input-group-append">
+                            <button id="button-text-color" class="btn btn-outline-input">Schriftfarbe</button>
+                        </div>
+                        <div class="input-group-append">
+                            <button id="button-upload" class="btn btn-outline-input">
+                                <img src="/svg/cloud-upload-fill.svg" alt="Hochladen-Symbol">
+                            </button>
+                        </div>
                     </div>
                 </div>
-                <input type="text" id="input-name" class="form-control" aria-labelledby="tag-name-prepend">
-                <div class="input-group-append">
-                    <input id="input-color" type="color" style="height: 100%;">
-                </div>
-                <div class="input-group-append">
-                    <button id="button-text-color" class="btn btn-outline-input">Schriftfarbe</button>
-                </div>
-                <div class="input-group-append">
-                    <button id="button-upload" class="btn btn-outline-input">
-                        <img src="/svg/cloud-upload-fill.svg" alt="Hochladen-Symbol">
+            </div>
+            <div class="row">
+                <div class="col">
+                    <button id="button-delete" data-toggle="modal" data-target="#delete-confirmation" class="btn btn-outline-input btn-danger">
+                        <img src="/svg/trash-fill-white.svg" alt="Mülleimer-Symbol">
                     </button>
                 </div>
             </div>
-            <button id="button-delete" data-toggle="modal" data-target="#delete-confirmation" class="btn btn-outline-input">
-                <img src="/svg/trash-fill-white.svg" alt="Mülleimer-Symbol">
-            </button>
         </div>
     </div>
 </div>
