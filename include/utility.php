2c8817e05eab8ad1480cb359bbd7bb0aab5188f6
Jeinzi <jeinzi@gmx.de>
2022-11-22 00:36:21 +0100

    Make database connect function more flexible

--- include/utility.php
+++ include/utility.php
@@ -6,41 +6,47 @@ function alert($text, $alertClass="alert-danger") {
 	echo '<button type="button" class="close" data-dismiss="alert" aria-label="Close">
 	<span aria-hidden="true">&times;</span>
   </button>';
-  	echo '</div>';
+  echo '</div>';
 }
 
 
 // Establishes a connection to the database and returns the connection object.
-function connectDb() {
-	// Read password from file.
-	$fileName = __DIR__ . "/password";
-	try {
-		$handle = fopen($fileName, "r");
-		if (!$handle) {
-			throw new Error("Can't open password file: '" . $fileName . "'");
-		}
-		$user = fgets($handle);
-		$password = fgets($handle);
-		fclose($handle);
-		$password = trim($password, "\n\r\t\0\x0B");
-		$user = trim($user, "\n\r\t\0\x0B");
-	}
-	catch (Error $e) {
-		alert($e->getMessage());
-		exit(1);
-	}
+// Expects a file containing the credentials:
+// - 1st line: database user
+// - 2nd line: database password
+// - 3rd line: database host, e.g. localhost
+function connectDb($authFile = __DIR__ . "/db-credentials") {
+  // Read password from file.
+  try {
+    $handle = fopen($authFile, "r");
+    if (!$handle) {
+      throw new Error("Can't open password file: '" . $authFile . "'");
+    }
+    $user = fgets($handle);
+    $password = fgets($handle);
+    $host = fgets($handle);
+    fclose($handle);
+  }
+  catch (Error $e) {
+    alert($e->getMessage());
+    exit(1);
+  }
+
+  $user = trim($user, "\n\r\t\0\x0B");
+  $password = trim($password, "\n\r\t\0\x0B");
+  $host = trim($host, "\n\r\t\0\x0B");
 
   try {
-		// The last parameter tells PDO to return the number of matched rows,
-		// even if they where not affected by an update because their value wasn't changed.
-		$db = new PDO('mysql:host=localhost;dbname=freya', $user, $password, array(PDO::MYSQL_ATTR_FOUND_ROWS => true));
-		$db->exec("SET NAMES 'utf8';");
+    // The last parameter tells PDO to return the number of matched rows,
+    // even if they where not affected by an update because their value wasn't changed.
+    $db = new PDO('mysql:host=' . $host . ';dbname=freya', $user, $password, array(PDO::MYSQL_ATTR_FOUND_ROWS => true));
+    $db->exec("SET NAMES 'utf8';");
   }
   catch (PDOException $e) {
-		alert("Could not establish connection to database: " . $e->getMessage());
-		exit(1);
+    alert("Could not establish connection to database: " . $e->getMessage());
+    exit(1);
   }
-	return($db);
+  return($db);
 }
 
 
